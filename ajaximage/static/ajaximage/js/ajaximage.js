4e0df7e848d5235bf599f2dd89b0e4f3c13c479d
Iacopo Spalletti <i.spalletti@nephila.it>
2014-06-17 10:38:42 +0200

    Use jquery

--- ajaximage/static/ajaximage/js/ajaximage.js
+++ ajaximage/static/ajaximage/js/ajaximage.js
@@ -1,6 +1,4 @@
-var $ajaxImage = jQuery.noConflict()
-
-$ajaxImage(function(){
+$(function(){
 
   var attach = function($fileInput, upload_url, el){
 
@@ -11,13 +9,13 @@ $ajaxImage(function(){
       paramName: 'file',
 
       add: function(e, data){
-        $ajaxImage(el).attr('class', 'ajaximage progress-active')
-        
+        $(el).attr('class', 'ajaximage progress-active')
+
         var regex = /jpg|jpeg|png|gif/i
-        
-        if ($ajaxImage(el).data('type') == 'image') {
+
+        if ($(el).data('type') == 'image') {
           if( ! regex.test(data.files[0].type)){
-            $ajaxImage(el).attr('class', 'ajaximage form-active')
+            $(el).attr('class', 'ajaximage form-active')
             return alert('Incorrect image format. Allowed (jpg, gif, png).')
           }
         }
@@ -27,49 +25,49 @@ $ajaxImage(function(){
 
       progress: function(e, data){
         var progress = parseInt(data.loaded / data.total * 100, 10)
-        $ajaxImage(el).find('.bar').css({width: progress + '%'})
+        $(el).find('.bar').css({width: progress + '%'})
       },
 
       error: function(e, data){
         alert('Oops, file upload failed, please try again')
-        $ajaxImage(el).attr('class', 'ajaximage form-active')
+        $(el).attr('class', 'ajaximage form-active')
       },
 
       done: function(e, data){
-        $ajaxImage(el).find('.link').attr('href', data.result.url)
-        $ajaxImage(el).find('img').attr('src', data.result.url)
-        $ajaxImage(el).attr('class', 'ajaximage img-active')
-        $ajaxImage(el).find('input[type=hidden]').val(data.result.filename)
-        $ajaxImage(el).find('.bar').css({width: '0%'})
+        $(el).find('.link').attr('href', data.result.url)
+        $(el).find('img').attr('src', data.result.url)
+        $(el).attr('class', 'ajaximage img-active')
+        $(el).find('input[type=hidden]').val(data.result.filename)
+        $(el).find('.bar').css({width: '0%'})
       }
     })
-  }
+  };
 
   var setup = function(el){
-    var upload_url = $ajaxImage(el).data('url')
-    var img_url = $ajaxImage(el).find('input[type=hidden]').val()
-    var $fileInput = $ajaxImage(el).find('input[type=file]')
-    
+    var upload_url = $(el).data('url');
+    var img_url = $(el).find('input[type=hidden]').val();
+    var $fileInput = $(el).find('input[type=file]');
+
     var class_ = (img_url === '') ? 'form-active' : 'img-active'
-    $ajaxImage(el).attr('class', 'ajaximage ' + class_)
-    
-    $ajaxImage(el).find('.remove').click(function(e){
+    $(el).attr('class', 'ajaximage ' + class_)
+
+    $(el).find('.remove').click(function(e){
       e.preventDefault()
-      $ajaxImage(el).find('input[type=hidden]').val('')
-      $ajaxImage(el).attr('class', 'ajaximage form-active')
-    })
+      $(el).find('input[type=hidden]').val('');
+      $(el).attr('class', 'ajaximage form-active')
+    });
 
     attach($fileInput, upload_url, el)
-  }
+  };
 
-  $ajaxImage('.ajaximage').each(function(i, el){
+  $('.ajaximage').each(function(i, el){
     setup(el)
-  })
+  });
 
-  $ajaxImage(document).bind('DOMNodeInserted', function(e) {
-    var el = $ajaxImage(e.target).find('.ajaximage').get(0)
-    var yes = $ajaxImage(el).length !== 0
+  $(document).bind('DOMNodeInserted', function(e) {
+    var el = $(e.target).find('.ajaximage').get(0);
+    var yes = $(el).length !== 0;
     if(yes) setup(el)
   })
 
-})
+});
